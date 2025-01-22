f4951e9c1b0ba36a400a6a3e8fe8eba3646e4c08
kagreene <kristengreene2023@U.northwestern.edu>
2025-01-21 19:10:42 -0600

    deleted folders"

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const {Model, DataTypes} = require('sequelize');
-const sequelizeConnection = require('../connection');
-class User extends Model {}
-User.init({
-    username: DataTypes.STRING,
-    password: DataTypes.STRING
-}, {sequelize: sequelizeConnection, modelName: 'user'});
-
-module.exports = User;
