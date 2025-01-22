1a212fa75825e965295d0f5e6006a1351549fc78
realejandro <realejandrocf@gmail.com>
2025-01-22 13:53:27 -0600

    creating home welcome carousel

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,19 @@
 export const Home = () => { 
     return (
     <section>
-      <button className="btn btn-primary">click</button>
+      <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="..." className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..." />
+          </div>
+        </div>
+      </div>
     </section>
 
   )
