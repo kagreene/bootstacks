2e7c5edccdf031ae2260a4f357e7a15aaccf669d
kagreene <kristengreene2023@U.northwestern.edu>
2025-01-23 21:01:13 -0600

    finished nflservice file

--- server/src/routes/api/nflApi.ts
+++ server/src/routes/api/nflApi.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import type { Request, Response } from 'express';
-import { getTeamSchedule } from '../../service/nflService.js'
+import { getTeamSchedule, getGameDetails } from '../../service/nflService.js'
 
 const router = express.Router();
 
-// GET /volunteers - Get all volunteers
-router.get('/', async (_req: Request, res: Response) => {
+// GET /team-schedule/:teamName - Get the schedule of games for a team
+router.get('/team-schedule/:teamName', async (req: Request, res: Response) => {
+  const teamName = req.params.teamName;
   try {
-    const volunteers = await Volunteer.findAll();
-    res.json(volunteers);
+    const schedule = await getTeamSchedule(teamName);
+    res.json(schedule);
+    
   } catch (error: any) {
     res.status(500).json({
       message: error.message
@@ -50,46 +52,6 @@ router.post('/', async (req: Request, res: Response) => {
   }
 });
 
-// PUT /volunteers/:id - Update a volunteer by ID
-router.put('/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { volunteerName } = req.body;
-  try {
-    const volunteer = await Volunteer.findByPk(id);
-    if(volunteer) {
-      volunteer.volunteerName = volunteerName;
-      await volunteer.save();
-      res.json(volunteer);
-    } else {
-      res.status(404).json({
-        message: 'Volunteer not found'
-      });
-    }
-  } catch (error: any) {
-    res.status(400).json({
-      message: error.message
-    });
-  }
-});
 
-// DELETE /volunteers/:id - Delete a volunteer by ID
-router.delete('/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const volunteer = await Volunteer.findByPk(id);
-    if(volunteer) {
-      await volunteer.destroy();
-      res.json({ message: 'User deleted' });
-    } else {
-      res.status(404).json({
-        message: 'User not found'
-      });
-    }
-  } catch (error: any) {
-    res.status(500).json({
-      message: error.message
-    });
-  }
-});
 
-export { router as volunteerRouter };
+export { router as nflRouter };
