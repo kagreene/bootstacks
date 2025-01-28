44ae0cd8a066d33270801d4c9ce8401c463e45a6
realejandro <realejandrocf@gmail.com>
2025-01-27 21:33:45 -0600

    deleting no desired keys sentences

--- client/src/api/testApi.tsx
+++ client/src/api/testApi.tsx
@@ -1,11 +1,4 @@
 
-//import from .env key
-//https://nfl-api-data.p.rapidapi.com/nfl-events?year=2023
-//https://nfl-api-data.p.rapidapi.com/nfl-single-events?id=401547401
-//Kristen key -> c51f2009e8mshc967a039cff7c74p12c78fjsn6c84dc76eb10
-//al -> a43cb93394mshb433c340a199dacp11e7b3jsn9c4a54948efa
-
-
 
 export const getEventsData = async() => {  
     const url = 'https://nfl-api-data.p.rapidapi.com/nfl-events?year=2025';
@@ -20,7 +13,6 @@ export const getEventsData = async() => {
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-        //console.log(result)
         console.log(result.events)
         const arrayEvents = result.events.map( ( objEvent:any ) => {
             return {
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: 3000, //3001
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
