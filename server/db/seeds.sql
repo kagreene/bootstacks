34e20bfda4ee8759643126598fc84e15643138e6
Pizza199 <steveha999@gmail.com>
2025-01-31 14:30:30 -0600

    registered users

--- server/db/seeds.sql
+++ server/db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO user( username, email, password )
+INSERT INTO users( username, email, password, "createdAt", "updatedAt")   
 VALUES 
-('Justin Rakestraw', 'jrakestraw@test.com', 'justin1234'),
-('Kristen Greene', 'kgreene@test.com', 'kristen1234'),
-('Steve Ha', 'sha@test.com', 'steve1234'),
-('Al Cabrera', 'alcabrera@test.com', 'alejandro1234');
+('Justin Rakestraw', 'jrakestraw@test.com', 'justin1234', NOW(), NOW()),
+('Kristen Greene', 'kgreene@test.com', 'kristen1234', NOW(), NOW()),
+('Steve Ha', 'sha@test.com', 'steve1234', NOW(), NOW()),
+('Al Cabrera', 'alcabrera@test.com', 'alejandro1234', NOW(), NOW());
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -30,9 +30,32 @@ export const login = async (req: Request, res: Response) => {
   return res.json({ token });
 };
 
+export const signup = async (req: Request, res: Response) => {
+
+  const { username, password, email } = req.body;
+  console.log(username + " " + password);
+  try{
+  const user = await User.create({
+    username: username,
+    password: password,
+    email: email
+  });
+    console.log(user);
+} catch (err) {
+    console.log('error from signup: ',err);
+    return res.status(401).json({ message: 'Signup failed' });
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+  return res.json({ token });
+};
+
 const router = Router();
 
 // POST /login - Login a user
 router.post('/login', login);
+router.post('/signup', signup);
 
 export default router;
